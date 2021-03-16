c8c70bcb09d6f03e9bc983d2ff02fc74e7c57daa
Kevin Reilley <kevinareilley@gmail.com>
2021-03-15 21:47:38 -0400

    Table working for 6th row! (headers)

--- src/gui/imguiGLWindow.cpp
+++ src/gui/imguiGLWindow.cpp
@@ -57,8 +57,17 @@ void GozihrWindow::Draw() {
     Disassemble(filePath);
   }
   // Display contents in a scrolling region
-  ImGui::BeginChild("Scrolling");
-  ImGui::Text("Working Area");
+  if (ImGui::BeginChild("Test Region"))
+  {
+  size_t q = 1;
+  const char* dstring = disasm.c_str();
+  size_t dlen = disasm.length();
+  std::string dlim = "\n";
+
+  if (ImGui::BeginChild("Scrolling")){
+  
+  ImGui::Text("Working Area 1");
+  ImGui::Separator();
   
   /* C++ substrings */
   /*
@@ -92,20 +101,89 @@ void GozihrWindow::Draw() {
 
   /* as a cstring */
   
-  const char* dstring = disasm.c_str();
-  int dlen = strlen(dstring);
+  
   char * teststr;
-  std::string dlim = "\n";
+  
+  
   teststr = new char[dlen + 1]();
   strncpy(teststr, dstring, dlen + 1);
   char * splitres = strtok(teststr, dlim.c_str());
   while (splitres != NULL){
-    ImGui::Text("%s", splitres);
+    //ImGui::Text("%s", splitres);
     splitres = strtok(NULL, dlim.c_str());
+    q++; // get max size of value vec
+  }
+  if(q>5){
+  ImGui::Text("Read Output");}
+  ImGui::Separator();
+  if (ImGui::BeginChild("Second Area")){
+    ImGui::Text("Working Area 2");
+  if (q>5)
+  {
+    char * valvec[q];
+    char * t2str;
+    char * target;
+    //char * thing = "address";
+    size_t r = 0;
+    t2str = new char[dlen + 1]();
+    strncpy(t2str, dstring, dlen + 1); // reload teststr
+    char * newres = strtok(t2str, dlim.c_str());
+    valvec[r] = newres;
+    r++;
+    while (newres != NULL){
+      //ImGui::Text("%s", splitres);
+      newres = strtok(NULL, dlim.c_str());
+      valvec[r] = newres;
+      r++; // get max size of value vec
+    }
+    //ImGui::Text("%s", valvec[5]);// 5 is the header row
+    char * headers[3];
+    size_t hndx = 0;
+    std::string hdlim = "|";
+    char * h3str = new char[strlen(valvec[5]) + 1]();
+    strncpy(h3str, valvec[5], strlen(valvec[5]) + 1);
+    char * hval = strtok(h3str, hdlim.c_str());
+    headers[hndx] = hval;
+    hndx++;
+    while (hval != NULL && hndx<4){
+      hval = strtok(NULL, hdlim.c_str());
+      headers[hndx] = hval;
+      hndx++;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+      ImGui::Text("%s", headers[i]);
+    }
+    ImGui::Separator();
+    static bool selected[3] = {};
+    if (ImGui::BeginTable("DisasmData",3))
+    {
+      ImGui::TableNextColumn(); 
+      for (int i = 0; i < 3; i++)
+                {
+                    char label[strlen(headers[i]+1)];
+                    sprintf(label, "%s", headers[i]);
+                    ImGui::Selectable(label, &selected[i]);
+                    ImGui::TableNextColumn(); // FIXME-TABLE: Selection overlap
+                }
+                ImGui::EndTable();
+    }
+    ImGui::Separator();
+
+
+
   }
   
-  
+  }
   ImGui::EndChild();
+  }
+  ImGui::EndChild();
+  
+  /*
+  }
+  */
+  
+  
   //ImGui::Text("..\n");
   /* Attempt at using getline */
   //std::vector<std::string> strs;
@@ -135,6 +213,8 @@ void GozihrWindow::Draw() {
   //ImGui::Text("%d", disasm.length());
   //ImGui::Text("%s", disasm.c_str());
   
+  }
+  ImGui::EndChild();
   ImGui::End();
 }
 
