035a7b0537e5d499c81f64f9b373081f282593cf
Kevin Reilley <kevinareilley@gmail.com>
2021-04-07 21:31:47 -0400

    removed bugged code
    removed bugged code. disassembles rust hello world in gui using old string API.

--- src/gui/imguiGLWindow.cpp
+++ src/gui/imguiGLWindow.cpp
@@ -68,43 +68,7 @@ void GozihrWindow::Draw() {
       
       ImGui::Text("Working Area 1");
       ImGui::Separator();
-    
-    /* C++ substrings */
-    /*
-    std::string workingcopy = disasm;
-    std::string d1 = "\n";
-    std::string d2 = "|";
-    size_t pos1 = 0;
-    size_t pos2 = 0;
-
-    std::string tok1;
-    std::string tok2;
-    bool found2=false;
-    
-    while ((pos1 = workingcopy.find(d1)) != std::string::npos) {
-      tok1 = workingcopy.substr(0, pos1);
-      std::string backup = tok1;
-      while ((pos2 = tok1.find(d2))!= std::string::npos){
-        found2 = true;
-        tok2 = tok1.substr(0, pos2);
-        ImGui::Text("%s", tok2);
-        tok1.erase(0, pos2 + d2.length());    
-      }
-      if (found2) {
-        ImGui::Text("%s", tok2);
-      }
-      // add last thing to figure
-      workingcopy.erase(0, pos1 + d1.length());
-    }
-    */ 
-
-
-    /* as a cstring */
-    
-    
     char * teststr;
-    
-    
     teststr = new char[dlen + 1]();
     strncpy(teststr, dstring, dlen + 1);
     char * splitres = strtok(teststr, dlim.c_str());
@@ -119,104 +83,7 @@ void GozihrWindow::Draw() {
     ImGui::EndChild();
     ImGui::TreePop();
     }
-    if (ImGui::TreeNode("Parsed Table"))
-    {
-    if (ImGui::BeginChild("Second Area")){
-      ImGui::Text("Working Area 2");
-    if (q>5)
-    {
-      char * valvec[q];
-      char * t2str;
-      char * target;
-      //char * thing = "address";
-      size_t r = 0;
-      t2str = new char[dlen + 1]();
-      strncpy(t2str, dstring, dlen + 1); // reload teststr
-      char * newres = strtok(t2str, dlim.c_str());
-      valvec[r] = newres;
-      r++;
-      while (newres != NULL){
-        //ImGui::Text("%s", splitres);
-        newres = strtok(NULL, dlim.c_str());
-        valvec[r] = newres;
-        r++; // get max size of value vec
-      }
-      //ImGui::Text("%s", valvec[5]);// 5 is the header row
-      char * headers[3];
-      size_t hndx = 0;
-      std::string hdlim = "|";
-      char * h3str = new char[strlen(valvec[5]) + 1]();
-      strncpy(h3str, valvec[5], strlen(valvec[5]) + 1);
-      char * hval = strtok(h3str, hdlim.c_str());
-      headers[hndx] = hval;
-      hndx++;
-      while (hval != NULL && hndx<4){
-        hval = strtok(NULL, hdlim.c_str());
-        headers[hndx] = hval;
-        hndx++;
-      }
-      for (int i = 0; i < 3; i++)
-      {
-        ImGui::Text("%s", headers[i]);
-      }
-      ImGui::Separator();
-      static bool selected[3] = {};
-      if (ImGui::BeginTable("DisasmData",3))
-      {
-        ImGui::TableNextColumn(); 
-        for (int i = 0; i < 3; i++)
-                  {
-                      char label[strlen(headers[i]+1)];
-                      sprintf(label, "%s", headers[i]);
-                      ImGui::Selectable(label, &selected[i]);
-                      ImGui::TableNextColumn(); 
-                  }
-                  ImGui::EndTable();
-      }
-      ImGui::Separator();
-      
-
-
     }
-    
-    }
-    ImGui::EndChild();
-    ImGui::TreePop();
-    }
-  }
-  ImGui::EndChild();
-  
-  
-  
-  //ImGui::Text("..\n");
-  /* Attempt at using getline */
-  //std::vector<std::string> strs;
-  //std::istringstream f(disasm);
-  //std::string s;
-  //while (std::getline(f, s, '\n')){
-  //std::istringstream g(s);
-  //std::string p;
-  //while (std::getline(g, p, '|')){
-  //  if (!p.empty()){ImGui::Text("%s",p);}
-  //}
-  /* Basic tests and text formatting */
-    //const char* currthing = s.c_str();
-    //int currlen = strlen(currthing);
-    //char* innerstr;
-    //innerstr = new char[currlen + 1]();
-    //strncpy(innerstr, currthing, currlen+1);    
-    //splitstring q(innerstr);
-    //std::vector<std::string> headers = q.split('|',0);
-    //for (int k = 0; k < headers.size(); k++){
-    //  ImGui::Text("%s", headers[k]);
-    //}
-    //strs.push_back(s);
-  //}
-  //ImGui::Text("..\n");
-  //ImGui::Text("%d", strlen(disasm.c_str()));
-  //ImGui::Text("%d", disasm.length());
-  //ImGui::Text("%s", disasm.c_str());
-  
   }
   ImGui::EndChild();
   ImGui::End();
